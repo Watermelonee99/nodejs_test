9bc0184015b1a84169b3fa8449bafdff8bf966c9
Watermelonee99 <tnqls331156@gmail.com>
2023-09-05 13:33:05 +0900

    0905-test

---
+++ app.js
@@ -0,0 +1,24 @@
+const  express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const apiRouter = require('./routes/routing')
+
+const app = express()
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://root:1234@mydb.a53fjz0.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true})
+  .then(() => console.log('Successfully connected to mongodb'))
+  .catch(e => console.error(e));
+
+app.set('views', path.resolve(__dirname + '/views'))
+app.set('view engine','ejs')
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+app.use('/',apiRouter)
+
+let port = 8008
+
+app.listen(port, () => {
+    console.log(`server is running at http://localhost:${port}`)
+})
